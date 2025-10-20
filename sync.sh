2fcee4d324e2de58545a2543a47fc5d281a0b141
Erik Hartin <erik@nhtbl.studio>
2025-10-20 17:27:45 +0100

    Find replace everyehwere

--- sync.sh
+++ sync.sh
@@ -38,7 +38,10 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   # wp "@$TO" db import ./temp_export_import.sql --path=web/wp &&
   cat ./temp_export_import.sql | wp "@$TO" db import - --path=web/wp && ## from dev
     echo "Modifying $TO db" &&
-  wp "@$TO" search-replace $FROMSITE $TOSITE --all-tables --precise --recurse-objects --skip-columns=guid --path=web/wp
+  wp "@$TO" search-replace $FROMSITE $TOSITE --all-tables --precise --recurse-objects --skip-columns=guid --path=web/wp &&
+  echo "Disabling automatic updates to prevent SSL errors" &&
+  wp "@$TO" config set AUTOMATIC_UPDATER_DISABLED true --type=constant --path=web/wp &&
+  wp "@$TO" config set WP_AUTO_UPDATE_CORE false --type=constant --path=web/wp
 fi
 if [[ "$uploads" =~ ^([yY][eE][sS]|[yY])$ ]]; then
   rsync -az --progress "$FROMDIR" "$TODIR"
